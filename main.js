be892a54fc979c0a57e32fdefa1345fcde3b1d93
Louver <revuol.19@gmail.com>
2022-12-08 09:46:07 -0800

    Modified Project 1

--- main.js
+++ main.js
@@ -16,20 +16,26 @@ export var newsList = [
   "Marie Colvin shined a light on war-torn corners of the world",
 ];
 
-export function search() {
+export function search(/*inputValue*/) {//Adding Parameter in search(inputValue) to test the function in main.test.js
   //inputValue is the variable that contains the search string
-  const inputValue = document.getElementById("search-input").value;
-
-  //Write your code here for the search function
-
+const inputValue = document.getElementById("search-input").value.toLowerCase();
+//Temporary disable the variable const inputValue to test the search function in main.test.js
+    for ( var i = 0 ; i < newsList.length ; i ++){
+      if (newsList[i].toLowerCase().includes(inputValue)){
+       newsList[i]
+       console.log(newsList[i])
+      }else{
+        newsList[i] = "";
+      }
+    }
   return newsList;
 }
 
 export function sort(type) {
   if (type == "ascending") {
-    //Write your code here for sorting (ascending)
+    newsList.sort();
   } else {
-    //Write your code here for sorting (descending)
+    newsList.reverse();
   }
 
   return newsList;
--- main.test.js
+++ main.test.js
@@ -1,6 +1,9 @@
 import { sort, search, newsList } from "./main.js";
-
 //Example testing for search function
 test("Search function testing", () => {
-  expect(search(newsList)).toBe("I am a cool web developer");
+  var i = 0 ; i <newsList.length ; i ++
+  expect(search("what")).toContain(newsList[i]);
+});
+test("Sort function testing", () => {
+  expect(sort(type)).toBe(sort());
 });
